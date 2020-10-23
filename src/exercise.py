590131ca2b249c0cf9b4bd5d378e3416626c8ace
DylanLlRees <19811@bridgend.ac.uk>
2020-10-23 13:15:42 +0000

    The programme works, however when tested I still get an error message.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,24 @@
 def main():
     #write your code below this line
 
+    # @Dylan - check your print statements against the README
+    
+    strmessage = input("Give a string:")
+    #strmessage = input("Give a string:")
+
+    print(strmessage)
+    intmessage = input("Give a integer:")
+    print(intmessage)
+    fltmessage = input("Give a float:")
+    print(fltmessage)
+    boolmessage = input("Give a boolean:")
+    print(boolmessage)
+    
+    print("You gave the string" + strmessage) 
+    #print("You gave the string " + strmessage)
+
+    print("You gave the integer" + intmessage)
+    print("You gave the float" + fltmessage)
+    print("You gave the boolean" + boolmessage)
 if __name__ == '__main__':
     main()
